95d3d669e60ce51bd45ca2e5aa105db3bce871b7
Wout Vandenbogaerde <wout.vandenbogaerde@student.hogent.be>
2022-02-22 17:10:36 +0100

    improved letter checking

--- js/woordje.js
+++ js/woordje.js
@@ -9,7 +9,7 @@
     var timeDiff = Math.abs(presentDate.getTime() - baseDate.getTime());
     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
     var baseValue = baseValue+diffDays;
-
+    var contains;
 
     startWoordje();
 
@@ -19,6 +19,7 @@
         letterIndex = 0;
         word = words[baseValue];
         wordLetters = word.split('');
+        contains = [...wordLetters];
         gameRunning = true;
         console.log(word)
     }
@@ -28,45 +29,58 @@
         if(letterIndex < 5 && gameRunning === true && grid.length != 6) {
             document.querySelector(`.tile:nth-of-type(${index + 1})`).textContent = guessedLetter.toUpperCase();;
             guessedWord.push(guessedLetter);
-            if(guessedLetter == wordLetters[letterIndex]) {
-                letterStatus.push("correct")
-            }
-            else if(wordLetters.includes(guessedLetter)) {
-                letterStatus.push("contains")
-            } else {
-                letterStatus.push("wrong")
-            }
+            
+
             index++;
             letterIndex++;
         }
     }
 
+    function removeItem(arr, value) {
+        var index = arr.indexOf(value);
+        if (index > -1) {
+          arr.splice(index, 1);
+        }
+        return arr;
+      }
+
     function nextChance() {
         letterIndex = 0
         guessedWord = [];
-        letterStatus = [];
     }
 
-    function checkWord(letterStatus, guessedWord) {
+    function checkWord(guessedWord) {
+        var colored = [...guessedWord]
+        console.log(wordLetters)
         if(guessedWord.length === 5) {
             if(words.includes(guessedWord.join("")) || allowedguesses.includes(guessedWord.join(""))) {
                 let c = 0;
-                for(var i = 0; i < letterStatus.length; i++) {
-                    if(letterStatus[i] == "correct") {
+                for(var i = 0; i < guessedWord.length; i++) {
+                    document.getElementById(guessedWord[i].toString()).style.backgroundColor = "#404040";
+                }
+                   
+                for(var i = 0; i < 5; i++) {
+                    if(wordLetters[i] === guessedWord[i] && contains.includes(guessedWord[i])) {
+                        contains = removeItem(contains, guessedWord[i])
                         c++;
                         document.querySelector(`.tile:nth-of-type(${index + i - 4})`).style.backgroundColor = "#45995b";
                         document.querySelector(`.tile:nth-of-type(${index + i - 4})`).style.border = "none";
                         document.getElementById(guessedWord[i]).style.backgroundColor = "#45995b";
                     } 
-                    else if(letterStatus[i] == "contains") {
+                }
+                for(var i = 0; i < 5; i++) {
+                    if(contains.includes(guessedWord[i])) {
+                        
+                        contains = removeItem(contains, guessedWord[i])
                         document.querySelector(`.tile:nth-of-type(${index + i - 4})`).style.backgroundColor = "#d4c65f";
                         document.querySelector(`.tile:nth-of-type(${index + i - 4})`).style.border = "none";
                         document.getElementById(guessedWord[i]).style.backgroundColor = "#d4c65f";
                     }
-                    else {
+                } 
+                    /*else {
                         document.getElementById(guessedWord[i]).style.backgroundColor = "#404040";
-                    }
-                }
+                    }*/
+                
                 
                 grid.push(letterStatus)
                 if(c === 5) {
@@ -89,10 +103,12 @@
                         }
                         msg += "%0a";
                     }
-                    document.getElementById("popupcontent").innerHTML = "Proficiat, je wint!</br><a style=' color: #fff;' href='http://twitter.com/intent/tweet?text=" + msg + "'>Tweet</a>";
+                    //document.getElementById("popupcontent").innerHTML = "Proficiat, je wint!</br><a style=' color: #fff;' href='http://twitter.com/intent/tweet?text=" + msg + "'>Tweet</a>";
+                    document.getElementById("popupcontent").innerHTML = "Proficiat, je wint!";
                         document.getElementById("popup").classList.add("win-animation");
                 } else if (grid.length === 6) {
-                    alert("helaas")
+                    document.getElementById("popupcontent").innerHTML = "Helaas, je hebt het woord niet geraden.";
+                        document.getElementById("popup").classList.add("win-animation");
                 }
                 
                 nextChance()
@@ -105,8 +121,10 @@
                         }, 2000);
                     }
                 }
+            }
+            contains = [...wordLetters];
         }
-    }
+    
 
     function backSpace(i, guessedWord) {
         if(guessedWord.length > 0) {
